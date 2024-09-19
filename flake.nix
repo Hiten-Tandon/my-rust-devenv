5a927c9946022c0b991a5327e686c6fcfbe00439
Hiten Tandon <tandonhiten@gmail.com>
2024-09-19 18:46:45 +0530

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description =
+    "A Nix flake with a rust environment for data science and visualization";
+
+  inputs = {
+    nixpkgs.url =
+      "github:NixOS/nixpkgs/nixos-unstable"; # Adjust this to the desired version
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { system = system; };
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ rustup pkg-config direnv ];
+          rustToolchains = [ "stable" "beta" "nightly" ];
+          shellHook = ''
+            for tc in $rustToolchains; do
+              rustup install $tc
+              rustup +$tc component add rust-analyzer
+              rustup +$tc component add clippy
+            done
+            nu
+            exit
+          '';
+        };
+      });
+}
+
